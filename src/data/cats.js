1b04da7f3758d619fb50b07a16c6c2ea476bc5e7
Rocky <rrsalerno21@gmail.com>
2020-07-22 15:37:01 -0700

    added test data

---
+++ src/data/cats.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet cat.  Loves belly rubs'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'gray',
+    weight: 15,
+    location: 'threed',
+    notes: 'Purrs constantly'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Zeus',
+    breed: 'afghan hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Katie',
+    breed: 'golden retriever',
+    gender: 'female',
+    age: 3,
+    color: 'black',
+    weight: 44,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    gender: 'femail',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'Super friendly'
+  }
+]
